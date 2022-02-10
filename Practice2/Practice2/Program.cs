ea8ce7886ec601492509737fe06fa5780f4b1d70
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-09 19:44:01 -0800

    Finish product

--- Practice2/Practice2/Program.cs
+++ Practice2/Practice2/Program.cs
@@ -2,6 +2,26 @@
 {
     static void Main ()
     {
+        //declare variables
+        int feet = 5;
+        int inches = 10;
+        double centimeter;
 
+        // 1 inch = 2.54 Centimeter
+        const double CentermeterConversion = 2.54;
+
+        //Convert feet into inches
+        int heightInInches = (feet * 12 ) + inches;
+
+        // Convert inches to centimeter
+        centimeter = heightInInches * CentermeterConversion;
+
+        // Display output
+        System.Console.Write ("Conversion from 5 feet 10 inches into centermeter is: ");
+        System.Console.Write(centimeter);
+        System.Console.Write(" cm");
+        System.Console.ReadKey();
+
+        
     }
 }
